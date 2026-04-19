5e08e3183b2020c72fa18faad63e09abc2f864da
hoangdm08 <dmhoang08t1@gmail.com>
2026-04-19 16:53:08 +0700

    delete

--- ST83/bt_1604/01_create_database.sql
+++
@@ -1,4 +0,0 @@
--- 01_create_database.sql
--- Tạo database cho hệ thống quản lý shop
-
-CREATE DATABASE manage_shopHDM;
--- ST83/bt_1608/01_create_database.sql
+++
@@ -1,7 +0,0 @@
--- 01_create_database.sql
--- Tạo database cho hệ thống quản lý bán hàng
-
-CREATE DATABASE QuanLyBanHang;
-
-USE QuanLyBanHang;
-DROP TABLE DonHangChiTiet;
